df93aabd4bc2424509cb5a9efa77475ded76833b
CyDrops <jbvhert2002@gmail.com>
2024-11-10 19:25:23 +0800

    chore: add missing #pragma once to header files

---
+++ src/cppFiles2/subjectTeacherQueue.h
@@ -0,0 +1,26 @@
+#pragma	once
+
+#include <cstdint>
+#include <iostream>
+#include <map>
+#include <queue>
+
+struct TeacherWorkload {
+	int id;
+	int max_work_load;
+
+	TeacherWorkload(int teacher_id, int workload)
+	    : id(teacher_id), max_work_load(workload) {}
+};
+
+class SubjectTeacherQueue {
+   private:
+	std::map<int, std::queue<TeacherWorkload>> queue;
+	std::map<int, std::vector<TeacherWorkload>> initial_state;
+
+   public:
+	void addTeacher(int subject_id, int teacher_id, int max_work_load);
+	TeacherWorkload* peekFrontTeacher(int subject_id);
+	int getTeacher(int subject_id, int decrement_work_load);
+	void resetQueue();
+};
