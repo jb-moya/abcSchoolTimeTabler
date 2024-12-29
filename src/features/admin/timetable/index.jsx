ecef6f47847a92d7c39d211af525bf9a8b6918bb
CyDrops <jbvhert2002@gmail.com>
2024-12-29 18:33:05 +0800

    feat: update generation of schedule

--- src/features/admin/timetable/index.jsx
+++ src/features/admin/timetable/index.jsx
@@ -36,6 +36,39 @@ import gcdOfArray from '../../../utils/getGCD';
 import { packThreeSignedIntsToInt32 } from '../../../utils/packThreeSignedIntsToInt32';
 const getTimetable = wrap(new WasmWorker());
 
+function addObjectToMap(
+    map,
+    key,
+    newObject,
+    subjectConfigurationIDIncrementer
+) {
+    if (!map.has(key)) {
+        map.set(key, []);
+    }
+
+    const currentArray = map.get(key);
+
+    const isDuplicate = currentArray.some((item) => {
+        let { subjectConfigurationID, ...rest } = item;
+        let { subjectConfigurationID: newSubjectConfigurationID, ...rest2 } =
+            newObject;
+
+        return deepEqual(rest, rest2);
+    });
+
+    if (!isDuplicate) {
+        currentArray.push({
+            subjectConfigurationID: subjectConfigurationIDIncrementer,
+            ...newObject,
+        });
+        map.set(key, currentArray);
+
+        return subjectConfigurationIDIncrementer + 1;
+    }
+
+    return subjectConfigurationIDIncrementer;
+}
+
 function Timetable() {
     const dispatch = useDispatch();
 
@@ -177,7 +210,7 @@ function Timetable() {
             {}
         );
 
-        const buildingMapReverse = Object.entries(mockBuildings).reduce(
+        const buildingMapReverse = Object.entries(buildingsStore).reduce(
             (acc, [, value], index) => {
                 acc[value.id] = index;
                 return acc;
@@ -202,27 +235,18 @@ function Timetable() {
             buildingMapReverse
         );
 
-        const buildingMap = Object.entries(mockBuildings).reduce(
+        const buildingMap = Object.entries(buildingsStore).reduce(
             (acc, [, building], index) => {
                 console.log('🚀 ~ handleButtonClick ~ building:', building);
 
                 acc[buildingMapReverse[building.id]] = {
                     id: buildingMapReverse[building.id],
 
-                    // adjacency: Array.isArray(building.nearbyBuildings)
-                    //     ? building.nearbyBuildings
-                    //           .map(
-                    //               (buildingID) =>
-                    //                   buildingMapReverse[buildingID.id] || null
-                    //           )
-                    //           .filter((building) => building !== null)
-                    //     : [],
-
                     adjacency: Array.isArray(building.nearbyBuildings)
                         ? building.nearbyBuildings
                               .map(
                                   (buildingID) =>
-                                      buildingMapReverse[buildingID] || null
+                                      buildingMapReverse[buildingID.id] ?? null
                               )
                               .filter((building) => building !== null)
                         : [],
@@ -273,6 +297,19 @@ function Timetable() {
         buildingInfoArray.push(-1);
         buildingAdjacencyArray.push(-1);
 
+        const teacherReservationConfigArray = [];
+        const teacherReservationConfigIDArray = [];
+
+        teacherReservationConfigArray.push(-1);
+        teacherReservationConfigIDArray.push(-1);
+
+        const teacherReservationConfig = new Int32Array([
+            ...teacherReservationConfigArray,
+        ]);
+        const teacherReservationConfigID = new Int32Array([
+            ...teacherReservationConfigIDArray,
+        ]);
+
         const buildingInfo = new Int32Array([...buildingInfoArray]);
         const buildingAdjacency = new Int32Array([...buildingAdjacencyArray]);
 
@@ -311,40 +348,13 @@ function Timetable() {
             {}
         );
 
-        function addObjectToMap(
-            map,
-            key,
-            newObject,
-            subjectConfigurationIDIncrementer
-        ) {
-            if (!map.has(key)) {
-                map.set(key, []);
-            }
+        const teacherReservedScheduleConfigurationMap = new Map();
+        let teacherReservedScheduleIDIncrementer = 0;
 
-            const currentArray = map.get(key);
-
-            const isDuplicate = currentArray.some((item) => {
-                let { subjectConfigurationID, ...rest } = item;
-                let {
-                    subjectConfigurationID: newSubjectConfigurationID,
-                    ...rest2
-                } = newObject;
-
-                return deepEqual(rest, rest2);
-            });
-
-            if (!isDuplicate) {
-                currentArray.push({
-                    subjectConfigurationID: subjectConfigurationIDIncrementer,
-                    ...newObject,
-                });
-                map.set(key, currentArray);
-
-                return subjectConfigurationIDIncrementer + 1;
-            }
-
-            return subjectConfigurationIDIncrementer;
-        }
+        Object.entries(teachersStore).forEach(([key, section]) => {
+            console.log('🚀 ~ Object.entries ~ teachersStore:', teachersStore);
+            // ...
+        });
 
         const subjectConfigurationMap = new Map();
         let subjectConfigurationIDIncrementer = 0;
@@ -463,6 +473,7 @@ function Timetable() {
         const subjectConfigurationSubjectDurationArray = [];
         const subjectConfigurationSubjectFixedTimeslotArray = [];
         const subjectConfigurationSubjectFixedDayArray = [];
+        const subjectConfigurationSubjectIsOverlappableArray = [];
         let totalNumOfSubjectConfigurations = 0;
 
         subjectConfigurationMap.forEach((subjectConfigurationArray) => {
@@ -492,6 +503,7 @@ function Timetable() {
                     subjectID,
                     subjectConfiguration.fixedDay
                 );
+                subjectConfigurationSubjectIsOverlappableArray[id] = 0;
 
                 totalNumOfSubjectConfigurations++;
             });
@@ -818,13 +830,14 @@ function Timetable() {
             // const notAllowedBreakslotGap = totalTimeslot >= 7 ? 2 : 1;
 
             let notAllowedBreakslotGap = 0;
-            if (totalTimeslot >= 5) {
+            if (totalTimeslot >= 5 && totalTimeslot < 7) {
+                notAllowedBreakslotGap = 1;
+            } else if (totalTimeslot >= 7) {
+                notAllowedBreakslotGap = 3;
+            } else if (totalTimeslot >= 10) {
                 notAllowedBreakslotGap = 3;
             }
-            // else if (totalTimeslot >= 7) {
-            //     notAllowedBreakslotGap = 3;
-            // }
-
+            
             const isDynamicSubjectConsistentDuration = 0;
 
             sectionConfigurationArray[sectionKey] = packInt8ToInt32(
@@ -875,6 +888,9 @@ function Timetable() {
         ]);
         const subjectConfigurationSubjectFixedDay = new Int32Array([
             ...subjectConfigurationSubjectFixedDayArray,
+        ]);
+        const subjectConfigurationSubjectIsOverlappable = new Int32Array([
+            ...subjectConfigurationSubjectIsOverlappableArray,
         ]);
         const sectionConfiguration = new Int32Array([
             ...sectionConfigurationArray,
@@ -936,9 +952,11 @@ function Timetable() {
             totalSectionSubjects: totalSectionSubjects,
             totalSection: totalSections,
             numberOfSubjectConfiguration: totalNumOfSubjectConfigurations,
+
             sectionConfiguration: sectionConfiguration,
 sectionLocation: sectionLocation,
             sectionSubjectConfiguration: sectionSubjectConfiguration,
+
             subjectConfigurationSubjectUnits: subjectConfigurationSubjectUnits,
             subjectConfigurationSubjectDuration:
                 subjectConfigurationSubjectDuration,
@@ -946,6 +964,9 @@ sectionLocation: sectionLocation,
                 subjectConfigurationSubjectFixedTimeslot,
             subjectConfigurationSubjectFixedDay:
                 subjectConfigurationSubjectFixedDay,
+subjectConfigurationSubjectIsOverlappable:
+                subjectConfigurationSubjectIsOverlappable,
+
             subjectFixedTeacherSection: subjectFixedTeacherSection,
             subjectFixedTeacher: subjectFixedTeacher,
             sectionStart: sectionStarts,
@@ -953,6 +974,10 @@ sectionLocation: sectionLocation,
             teacherWeekLoadConfig: teacherWeekLoadConfig,
 buildingInfo: buildingInfo,
             buildingAdjacency: buildingAdjacency,
+
+            teacherReservationConfig: teacherReservationConfig,
+            teacherReservationConfigID: teacherReservationConfigID,
+
             teacherSubjectsLength: teacherSubjects.length,
             beesPopulation: beesPopulations,
             beesEmployed: beesEmployed,
@@ -960,6 +985,7 @@ buildingInfo: buildingInfo,
             beesScout: beesScout,
             limit: limits,
             workWeek: numOfSchoolDays,
+
             breakTimeDuration: breakTimeDuration,
             teacherBreakThreshold: teacherBreakThreshold,
             teacherMiddleTimePointGrowAllowanceForBreakTimeslot:
@@ -970,6 +996,7 @@ buildingInfo: buildingInfo,
             offsetDuration: offset,
             resultTimetableLength: resultTimetableLength,
             resultViolationLength: resultViolationLength,
+
             enableLogging: false,
         };
 
