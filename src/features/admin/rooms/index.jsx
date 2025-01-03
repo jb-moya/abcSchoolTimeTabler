a664503d9103ab57c105439956373def1e7689fd
CyDrops <jbvhert2002@gmail.com>
2025-01-03 10:46:43 +0800

    style: code formatting

--- src/features/admin/rooms/index.jsx
+++ src/features/admin/rooms/index.jsx
@@ -1,33 +1,27 @@
 import Breadcrumbs from '@components/Admin/Breadcrumbs';
 import RoomMapping from '@components/Admin/RoomsAndBuildings/RoomMapping';
 
-
 function Rooms() {
+    const links = [
+        { name: 'Home', href: '/' },
+        // { name: 'Modify Subjects', href: '/modify-subjects' },
+    ];
 
-  const links = [
-    { name: 'Home', href: '/' },
-    // { name: 'Modify Subjects', href: '/modify-subjects' },
-  ];
+    return (
+        <div className="App container mx-auto px-4 mb-10">
+            <Breadcrumbs title="Room Mapping" links={links} />
 
-  return (
-    <div className="App container mx-auto px-4 mb-10">
-
-    <Breadcrumbs title="Room Mapping" links={links} />
-  
-    {/* Main Content */}
-    <div className="flex flex-col gap-4">
-      <div className="card w-full bg-base-100 hadow-md">
-        <div className="card-body">
-            {/* <DepartmentListContainer editable={true} />  */}
-            <RoomMapping  editable={true} />
+            {/* Main Content */}
+            <div className="flex flex-col gap-4">
+                <div className="card w-full bg-base-100 hadow-md">
+                    <div className="card-body">
+                        {/* <DepartmentListContainer editable={true} />  */}
+                        <RoomMapping editable={true} />
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-  
-    </div>
-  </div>
-  );
-
- 
+    );
 }
 
 export default Rooms;
