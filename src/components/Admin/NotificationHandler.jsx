eec3613b2d588488b6e0de23423efecd34548649
CyDrops <jbvhert2002@gmail.com>
2025-01-05 22:43:11 +0800

    refactor: adhere to prettier standard

--- src/components/Admin/NotificationHandler.jsx
+++ src/components/Admin/NotificationHandler.jsx
@@ -1,57 +1,57 @@
-import { useEffect, useRef } from "react";
-import notif from "@assets/notif.mp3";
+import { useEffect, useRef } from 'react';
+import notif from '@assets/notif.mp3';
 
 const NotificationHandler = ({ timetableCondition }) => {
-  const notifTitle = "Timetabler";
-  const notifBody = "Your timetable is ready!";
-  const originalTitle = useRef(document.title);
-  const timeoutId = useRef(null);
+    const notifTitle = 'Timetabler';
+    const notifBody = 'Your timetable is ready!';
+    const originalTitle = useRef(document.title);
+    const timeoutId = useRef(null);
 
-  useEffect(() => {
-    const handleVisibilityChange = () => {
-      if (!document.hidden && timetableCondition === "success") {
-        // User returned to the tab
-        timeoutId.current = setTimeout(() => {
-          document.title = originalTitle.current;
-        }, 1000);
-      }
-    };
+    useEffect(() => {
+        const handleVisibilityChange = () => {
+            if (!document.hidden && timetableCondition === 'success') {
+                // User returned to the tab
+                timeoutId.current = setTimeout(() => {
+                    document.title = originalTitle.current;
+                }, 1000);
+            }
+        };
 
-    if (timetableCondition === "success" && document.hidden) {
-      document.title = "Timetable ready!";
-      const audio = new Audio(notif);
-      audio.volume = 0.4;
-      audio.play();
+        if (timetableCondition === 'success' && document.hidden) {
+            document.title = 'Timetable ready!';
+            const audio = new Audio(notif);
+            audio.volume = 0.4;
+            audio.play();
 
-      if (Notification.permission === "granted") {
-        new Notification(notifTitle, {
-          body: notifBody,
-        });
-      } else if (Notification.permission !== "denied") {
-        Notification.requestPermission().then((permission) => {
-          if (permission === "granted") {
-            new Notification(notifTitle, {
-              body: notifBody,
-            });
-          }
-        });
-      }
-    } else if (timetableCondition === "running") {
-      document.title = "Generating...";
-    }
+            if (Notification.permission === 'granted') {
+                new Notification(notifTitle, {
+                    body: notifBody,
+                });
+            } else if (Notification.permission !== 'denied') {
+                Notification.requestPermission().then((permission) => {
+                    if (permission === 'granted') {
+                        new Notification(notifTitle, {
+                            body: notifBody,
+                        });
+                    }
+                });
+            }
+        } else if (timetableCondition === 'running') {
+            document.title = 'Generating...';
+        }
 
-    document.addEventListener("visibilitychange", handleVisibilityChange);
+        document.addEventListener('visibilitychange', handleVisibilityChange);
 
-    return () => {
-      document.title = originalTitle.current;
-      document.removeEventListener("visibilitychange", handleVisibilityChange);
-      if (timeoutId.current) {
-        clearTimeout(timeoutId.current);
-      }
-    };
-  }, [timetableCondition]);
+        return () => {
+            document.title = originalTitle.current;
+            document.removeEventListener('visibilitychange', handleVisibilityChange);
+            if (timeoutId.current) {
+                clearTimeout(timeoutId.current);
+            }
+        };
+    }, [timetableCondition]);
 
-  return null;
+    return null;
 };
 
 export default NotificationHandler;
--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 // import { WasmProvider } from "./WasmContext.jsx";
-import store from "./store/store";
-import { Provider } from "react-redux";
-import { Toaster, toast} from "sonner";
+import store from './store/store';
+import { Provider } from 'react-redux';
+import { Toaster, toast } from 'sonner';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    {/* <WasmProvider> */}
-    <Toaster />
-    <App />
-    {/* </WasmProvider> */}
-  </Provider>
-  // </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+    // <React.StrictMode>
+    <Provider store={store}>
+        {/* <WasmProvider> */}
+        <Toaster richColors position='top-right' />;
+        <App />
+        {/* </WasmProvider> */}
+    </Provider>
+    // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
