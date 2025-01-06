526288344a4e458e69eabd723a4954ce100ee90f
CyDrops <jbvhert2002@gmail.com>
2025-01-06 12:18:16 +0800

    feat: update icons

--- src/routes/sidebar.jsx
+++ src/routes/sidebar.jsx
@@ -1,94 +1,82 @@
-/** Icons are imported separatly to reduce build time */
-import BellIcon from '@heroicons/react/24/outline/BellIcon';
-import DocumentTextIcon from '@heroicons/react/24/outline/DocumentTextIcon';
 import Squares2X2Icon from '@heroicons/react/24/outline/Squares2X2Icon';
-import TableCellsIcon from '@heroicons/react/24/outline/TableCellsIcon';
-import WalletIcon from '@heroicons/react/24/outline/WalletIcon';
-import CodeBracketSquareIcon from '@heroicons/react/24/outline/CodeBracketSquareIcon';
-import DocumentIcon from '@heroicons/react/24/outline/DocumentIcon';
-import ExclamationTriangleIcon from '@heroicons/react/24/outline/ExclamationTriangleIcon';
-import CalendarDaysIcon from '@heroicons/react/24/outline/CalendarDaysIcon';
-import ArrowRightOnRectangleIcon from '@heroicons/react/24/outline/ArrowRightOnRectangleIcon';
-import UserIcon from '@heroicons/react/24/outline/UserIcon';
-import Cog6ToothIcon from '@heroicons/react/24/outline/Cog6ToothIcon';
-import BoltIcon from '@heroicons/react/24/outline/BoltIcon';
-import ChartBarIcon from '@heroicons/react/24/outline/ChartBarIcon';
-import CurrencyDollarIcon from '@heroicons/react/24/outline/CurrencyDollarIcon';
-import InboxArrowDownIcon from '@heroicons/react/24/outline/InboxArrowDownIcon';
 import UsersIcon from '@heroicons/react/24/outline/UsersIcon';
 import Table from '@heroicons/react/24/outline/TableCellsIcon';
 
-import KeyIcon from '@heroicons/react/24/outline/KeyIcon';
-import DocumentDuplicateIcon from '@heroicons/react/24/outline/DocumentDuplicateIcon';
+import { MdOutlineMeetingRoom } from 'react-icons/md';
+import { HiOutlineOfficeBuilding } from 'react-icons/hi';
+import { PiBooksThin } from 'react-icons/pi';
+import { GrSchedules } from 'react-icons/gr';
+import { FaChalkboardTeacher } from 'react-icons/fa';
+import { GrGroup } from 'react-icons/gr';
 
 const iconClasses = `h-6 w-6`;
 const submenuIconClasses = `h-5 w-5`;
 
 const routes = [
-  {
-    path: '/app/dashboard',
-    icon: <Squares2X2Icon className={iconClasses} />,
-    name: 'Dashboard',
-  },
-  {
-    path: '', //no url needed as this has submenu
-    icon: <Table className={`${iconClasses} inline`} />, // icon component
-    name: 'Timetable', // name that appear in Sidebar
-    submenu: [
-      {
-        path: '/app/admin/generate-timetable',
-        icon: <ArrowRightOnRectangleIcon className={submenuIconClasses} />,
-        name: 'Generate Timetable',
-      },
-      {
-        path: '/app/admin/modify-subjects', //url
-        icon: <UserIcon className={submenuIconClasses} />, // icon component
-        name: 'Modify Subjects and Programs', // name that appear in Sidebar
-      },
-      {
-        path: '/app/admin/modify-teachers',
-        icon: <KeyIcon className={submenuIconClasses} />,
-        name: 'Modify Teachers',
-      },
-      {
-        path: '/app/admin/modify-sections',
-        icon: <DocumentIcon className={submenuIconClasses} />,
-        name: 'Modify Sections',
-      },
-      {
-        path: '/app/admin/modify-departments',
-        icon: <DocumentIcon className={submenuIconClasses} />,
-        name: 'Modify Departments',
-      },
-      {
-        path: '/app/admin/room-mapping',
-        icon: <DocumentIcon className={submenuIconClasses} />,
-        name: 'Room Utilization',
-      },
-    ],
-  },
-  {
-    path: '', //no url needed as this has submenu
-    icon: <Cog6ToothIcon className={`${iconClasses} inline`} />, // icon component
-    name: 'Report Schedules', // name that appear in Sidebar
-    submenu: [
-      {
-        path: '/app/reports/display-teachers-schedule', //url
-        icon: <UserIcon className={submenuIconClasses} />, // icon component
-        name: 'Teacher Schedule', // name that appear in Sidebar
-      },
-      {
-        path: '/app/reports/display-sections-schedule',
-        icon: <WalletIcon className={submenuIconClasses} />,
-        name: 'Section Schedule',
-      },
-      {
-        path: '/app/reports/display-subjects-schedule', // url
-        icon: <UsersIcon className={submenuIconClasses} />, // icon component
-        name: 'Subject Schedule', // name that appear in Sidebar
-      },
-    ],
-  },
+    {
+        path: '/app/dashboard',
+        icon: <Squares2X2Icon className={iconClasses} />,
+        name: 'Dashboard',
+    },
+    {
+        path: '',
+        icon: <Table className={`${iconClasses} inline`} />, 
+        name: 'Timetable',
+        submenu: [
+            {
+                path: '/app/admin/generate-timetable',
+                icon: <GrSchedules className={submenuIconClasses} />,
+                name: 'Generate Timetable',
+            },
+            {
+                path: '/app/admin/modify-subjects',
+                icon: <PiBooksThin className={submenuIconClasses} />, 
+                name: 'Modify Subjects and Programs',
+            },
+            {
+                path: '/app/admin/modify-teachers',
+                icon: <FaChalkboardTeacher className={submenuIconClasses} />,
+                name: 'Modify Teachers',
+            },
+            {
+                path: '/app/admin/modify-sections',
+                icon: <GrGroup className={submenuIconClasses} />,
+                name: 'Modify Sections',
+            },
+            {
+                path: '/app/admin/modify-departments',
+                icon: <HiOutlineOfficeBuilding className={submenuIconClasses} />,
+                name: 'Modify Departments',
+            },
+            {
+                path: '/app/admin/room-mapping',
+                icon: <MdOutlineMeetingRoom className={submenuIconClasses} />,
+                name: 'Room Utilization',
+            },
+        ],
+    },
+    {
+        path: '',
+        icon: <GrSchedules className={`${iconClasses} inline`} />, 
+        name: 'Report Schedules',
+        submenu: [
+            {
+                path: '/app/reports/display-teachers-schedule',
+                icon: <FaChalkboardTeacher className={submenuIconClasses} />, 
+                name: 'Teacher Schedule',
+            },
+            {
+                path: '/app/reports/display-sections-schedule',
+                icon: <GrGroup className={submenuIconClasses} />,
+                name: 'Section Schedule',
+            },
+            {
+                path: '/app/reports/display-subjects-schedule',
+                icon: <UsersIcon className={submenuIconClasses} />, 
+                name: 'Subject Schedule',
+            },
+        ],
+    },
 ];
 
 export default routes;
