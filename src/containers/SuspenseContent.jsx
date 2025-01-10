495d558d3b307f621e9de82b81262af396b66134
CyDrops <jbvhert2002@gmail.com>
2025-01-10 23:15:59 +0800

    feat: improve loading UI

--- src/containers/SuspenseContent.jsx
+++ src/containers/SuspenseContent.jsx
@@ -1,9 +1,10 @@
-function SuspenseContent(){
-    return(
-        <div className="w-full h-screen text-gray-300 dark:text-gray-200 bg-base-100">
-            Loading...
+function SuspenseContent() {
+    return (
+        <div className='absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 z-50 dark:text-gray-200 items-center justify-center flex flex-col gap-2'>
+            <span className='loading loading-bars loading-lg'></span>
+            <span>loading...</span>
         </div>
-    )
+    );
 }
 
-export default SuspenseContent
+export default SuspenseContent;
