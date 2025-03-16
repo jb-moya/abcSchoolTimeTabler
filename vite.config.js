4eaca426f9a8e4185acaa41404cd2b461414ac3d
CyDrops <jbvhert2002@gmail.com>
2025-03-16 21:20:38 +0800

    chore: update build configuration

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path';
 export default defineConfig({
     plugins: [react()],
     server: { port: 8001 },
+    base: process.env.VITE_BASE_PATH || "abcSchoolTimeTabler",
     resolve: {
         alias: {
             '@components': path.resolve(__dirname, 'src/components'),
