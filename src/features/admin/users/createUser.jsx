dde40daf493cd6b482e5bb34fbc6f77cce67a5f0
CyDrops <jbvhert2002@gmail.com>
2025-03-21 07:58:20 +0800

    feat: update create user form

--- src/features/admin/users/createUser.jsx
+++ src/features/admin/users/createUser.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useState } from 'react';
-// import { useForm } from 'react-hook-form';
 import InputText from '../../../components/Input/InputText';
+import { useDispatch, useSelector } from 'react-redux';
+import { signUpWithEmailAndPassword } from '../../userSlice';
 
 const submenu = [
     'Generate Timetable',
@@ -13,81 +14,127 @@ const submenu = [
     'Modify TimeTable',
 ];
 
-const roles = [
-    "superman",
-    "minions"
-]
-
 const CreateUser = () => {
     const INITIAL_REGISTER_OBJ = {
         email: '',
         password: '',
+        confirmPassword: '',
         permissions: [],
         role: '',
     };
 
+    const dispatch = useDispatch();
+
+    const { user, error: userError, status: userStatus } = useSelector((state) => state.user);
     const [registerObj, setRegisterObj] = useState(INITIAL_REGISTER_OBJ);
+
+    const handleCheckboxChange = (event, routeName) => {
+        const { value, checked } = event.target;
+        console.log("🚀 ~ handleCheckboxChange ~ checked:", checked, value)
+        const updatedPermissions = checked
+            ? [...registerObj.permissions, routeName] // Add if checked
+            : registerObj.permissions.filter((perm) => perm !== routeName); // Remove if unchecked
+
+        updateFormValue({ updateType: 'permissions', value: updatedPermissions });
+    };
 
     const updateFormValue = ({ updateType, value }) => {
         setRegisterObj({ ...registerObj, [updateType]: value });
     };
 
-    const [selectedPermissions, setSelectedPermissions] = useState([]);
-    const [selectedRoles, setSelectedRoles] = useState([]);
+    const submitForm = (e) => {
+        e.preventDefault();
 
-    const handleSubmit = () => {};
+        try {
+            const result = dispatch(signUpWithEmailAndPassword(registerObj));
 
-    const onSubmit = (data) => {
-        console.log({ ...data, permissions: selectedPermissions, roles: selectedRoles });
+            if (result.meta.requestStatus === 'rejected') {
+                return;
+            }
+
+            console.log('successfully registered');
+        } catch (error) {}
     };
 
     return (
         <div>
-            <form onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
-                <div>
-                    <InputText
-                        defaultValue={registerObj.email}
-                        updateType='email'
-                        containerStyle=''
-                        labelTitle='Email'
-                        updateFormValue={updateFormValue}
-                    />
+            <form onSubmit={(e) => submitForm(e)} className='space-y-2'>
+                <div className='divider divider-start'>
+                    Account Details <span className='text-sm'></span>
                 </div>
 
-                <div>
-                    <InputText
-                        defaultValue={registerObj.password}
-                        type='password'
-                        updateType='password'
-                        containerStyle=''
-                        labelTitle='Password'
-                        updateFormValue={updateFormValue}
-                    />
+                <div className='w-96'>
+                    <div>
+                        <InputText
+                            defaultValue={registerObj.email}
+                            updateType='email'
+                            containerStyle=''
+                            labelTitle='Email'
+                            updateFormValue={updateFormValue}
+                        />
+                    </div>
+
+                    <div>
+                        <InputText
+                            defaultValue={registerObj.password}
+                            type='password'
+                            updateType='password'
+                            containerStyle=''
+                            labelTitle='Password'
+                            updateFormValue={updateFormValue}
+                        />
+                    </div>
+
+                    <div>
+                        <InputText
+                            defaultValue={registerObj.confirmPassword}
+                            type='password'
+                            updateType='confirmPassword'
+                            containerStyle=''
+                            labelTitle='Confirm Password'
+                            updateFormValue={updateFormValue}
+                        />
+                    </div>
                 </div>
 
-                <div className=''>
+                <div className='pt-6'></div>
+                <div className='divider divider-start'>
+                    Permissions <span className='text-sm'>grant user access to the following</span>
+                </div>
+
+                <div className='space-y-1'>
                     {submenu.map((routeName, index) => (
                         <div key={index} className='flex gap-2'>
-                            <input type='checkbox' className='checkbox' />
+                            <input
+                                type='checkbox'
+                                className='checkbox checkbox-success'
+                                onChange={(e) => handleCheckboxChange(e, routeName)}
+                            />
                             <div>{routeName}</div>
                         </div>
                     ))}
                 </div>
 
-                <div className='form-control'>
+                <div className='pt-6'></div>
+                <div className='divider divider-start'>
+                    Role <span className='text-sm'></span>
+                </div>
+
+                <div className='form-control w-52'>
                     <label className='label cursor-pointer'>
                         <span className='label-text'>Superman</span>
                         <input type='radio' name='radio-10' className='radio checked:bg-red-500' defaultChecked />
                     </label>
                 </div>
-                <div className='form-control'>
+                <div className='form-control w-52'>
                     <label className='label cursor-pointer'>
                         <span className='label-text'>Minions</span>
                         <input type='radio' name='radio-10' className='radio checked:bg-blue-500' defaultChecked />
                     </label>
                 </div>
 
-                {/* <button
+                <div className='pt-6'></div>
+                <button
                     type='submit'
                     className={`btn mt-4 w-full btn-primary text-white transition-all duration-75 ease-in-out flex items-center justify-center ${
                         userStatus == 'loading' ? 'cursor-not-allowed ' : ''
@@ -102,7 +149,7 @@ const CreateUser = () => {
                     ) : (
                         'Sign Up'
                     )}
-                </button> */}
+                </button>
             </form>
         </div>
     );
--- src/features/admin/users/index.jsx
+++ src/features/admin/users/index.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
 import CreateUser from './createUser';
+import Breadcrumbs from '@components/Admin/Breadcrumbs';
+
 const Users = () => {
+    const links = [{ name: 'Home', href: '/' }];
+
     return (
-        <div role='tablist' className='tabs tabs-lifted'>
-            <input type='radio' name='my_tabs_2' role='tab' className='tab' aria-label='User Dashboard' />
-            <div role='tabpanel' className='tab-content bg-base-100 border-base-300 rounded-box p-6'>
-                Tab content 1
-            </div>
+        <div className='App container mx-auto px-4 mb-10'>
+            <Breadcrumbs title='Users' links={links} />
 
-            <input type='radio' name='my_tabs_2' role='tab' className='tab' aria-label='Create New User' defaultChecked />
-            <div role='tabpanel' className='tab-content bg-base-100 border-base-300 rounded-box p-6'>
-                <CreateUser />
-            </div>
+            <div role='tablist' className='tabs tabs-lifted tabs-lg'>
+                <input type='radio' name='my_tabs_2' role='tab' className='tab' aria-label='User Dashboard' />
+                <div role='tabpanel' className='tab-content bg-base-100 border-base-300 rounded-box p-6'>
+                    Tab content 1
+                </div>
 
-            <input type='radio' name='my_tabs_2' role='tab' className='tab' aria-label='Edit User' defaultChecked />
-            <div role='tabpanel' className='tab-content bg-base-100 border-base-300 rounded-box p-6'>
-                Edit
-            </div>
+                <input type='radio' name='my_tabs_2' role='tab' className='tab' aria-label='Create New User' defaultChecked />
+                <div role='tabpanel' className='tab-content bg-base-100 border-base-300 rounded-box p-6'>
+                    <CreateUser />
+                </div>
 
-            <input type='radio' name='my_tabs_2' role='tab' className='tab' aria-label='User Logs' />
-            <div role='tabpanel' className='tab-content bg-base-100 border-base-300 rounded-box p-6'>
-                Tab content 3
+                <input type='radio' name='my_tabs_2' role='tab' className='tab' aria-label='Edit User' defaultChecked />
+                <div role='tabpanel' className='tab-content bg-base-100 border-base-300 rounded-box p-6'>
+                    Edit
+                </div>
+
+                <input type='radio' name='my_tabs_2' role='tab' className='tab' aria-label='User Logs' />
+                <div role='tabpanel' className='tab-content bg-base-100 border-base-300 rounded-box p-6'>
+                    Tab content 3
+                </div>
             </div>
         </div>
     );
